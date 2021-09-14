961880afdd1263133ed25635f4a5bd3f7c20f1b4
Ayushjain1722 <42894689+Ayushjain1722@users.noreply.github.com>
2021-09-14 20:09:48 +0530

    Chromedriver path is set

--- linkedinScraper.py
+++ linkedinScraper.py
@@ -6,8 +6,16 @@ from bs4 import BeautifulSoup
 import json
 
 def signIn():
+    GOOGLE_CHROME_PATH = '/app/.apt/usr/bin/google_chrome'
+    CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
     
-    browser = webdriver.Chrome("chromedriver.exe")
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.binary_location = GOOGLE_CHROME_PATH
+    
+
+    browser = webdriver.Chrome(execution_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
 
     browser.maximize_window()
     browser.get('https://www.linkedin.com/uas/login')
