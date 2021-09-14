e99a69e3ea37ab59a29611e96d23185f29283ae6
Ayushjain1722 <42894689+Ayushjain1722@users.noreply.github.com>
2021-09-14 20:50:13 +0530

    Removed headless argument

--- linkedinScraper.py
+++ linkedinScraper.py
@@ -13,7 +13,6 @@ def signIn():
     chrome_options.binary_location = chrome_bin
     chrome_options.add_argument('--disable-gpu')
     chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.add_argument('--remote-debugging-port=9222')
     chrome_options.add_argument('--disable-infobars')
